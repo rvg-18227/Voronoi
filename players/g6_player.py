4c9668b8eaffa5c05a136b0fbc3a22040b35aa6b
Micah Cheng <heymicahme@gmail.com>
2022-10-04 18:30:22 -0400

    Initial diagonal

--- players/g6_player.py
+++ players/g6_player.py
@@ -4,7 +4,7 @@ import numpy as np
 import sympy
 import logging
 from typing import Tuple
-
+from sympy.geometry import Point2D
 
 class Player:
     def __init__(self, rng: np.random.Generator, logger: logging.Logger, total_days: int, spawn_days: int,
@@ -42,6 +42,8 @@ class Player:
         self.rng = rng
         self.logger = logger
 
+        self.player_idx = player_idx
+
     def play(self, unit_id, unit_pos, map_states, current_scores, total_scores) -> [tuple[float, float]]:
         """Function which based on current game state returns the distance and angle of each unit active on the board
 
@@ -60,7 +62,42 @@ class Player:
                     List[Tuple[float, float]]: Return a list of tuples consisting of distance and angle in radians to
                                                 move each unit of the player
                 """
-
+        self.map_states = map_states
         moves = []
-
+        for unit in unit_pos[self.player_idx]:
+            move = self.transform_move(np.arctan2(1, 1))
+            if self.check_move(unit, move) == self.player_idx:
+                moves.append(self.transform_move(0, 0))
+            moves.append(move)
         return moves
+
+    def check_move(self, unit_pos, move) -> int:
+        """Checks if the move is valid and returns the player index of the unit occupying the cell
+                Args:
+                    unit_pos (Tuple(float, float)): current position of the unit
+                    move (Tuple(float, float)): move to be made as (distance, angle)
+
+                Returns:
+                    int: player index of the player who owns the cell
+        """
+        distance, angle = move
+        end_pos = (unit_pos[0] + distance * np.cos(angle), unit_pos[1] + distance * np.sin(angle))
+        return self.map_states[np.floor(end_pos[0])][np.floor(end_pos[1])]
+
+
+    def transform_move(self, angle: float, distance=1) -> tuple[float, float]:
+        """Transforms the distance and angle to the correct format for the game engine
+                Args:
+                    angle (float): angle in radians
+                    distance (float): distance to move
+                Returns
+                    Tuple[float, float]: distance and angle in correct format
+        """
+        if self.player_idx == 0:
+            return (distance, angle)
+        elif self.player_idx == 1:
+            return (distance, angle - np.pi/2)
+        elif self.player_idx == 2:
+            return (distance, angle + np.pi)
+        else:# self.player_idx == 3:
+            return (distance, angle + np.pi / 2)
