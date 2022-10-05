ce16a6fd02869e0dc03cd8fe55b99844e316cbed
yunlanli <chnsidney@gmail.com>
2022-10-04 20:46:25 -0400

    remember player index, pass correct arguments to get_moves

--- players/g3_player.py
+++ players/g3_player.py
@@ -3,7 +3,7 @@ import pickle
 import numpy as np
 import sympy
 import logging
-from typing import Tuple
+from typing import Tuple, List
 import math
 
 
@@ -83,10 +83,12 @@ class Player:
 
         self.rng = rng
         self.logger = logger
+        
+        self.us = player_idx
 
         self.target_loc = []
 
-        initial_radius = 25
+        self.initial_radius = 25
 
         base_angle = (-1) * ((math.pi / 2) + (player_idx * math.pi/2))
 
@@ -96,10 +98,11 @@ class Player:
         #    |     
         #    -------> (base angle): -pi/2 - pi/2 * (player_index)
         # p3
-        outer_wall_angles = np.linspace(start=base_angle, end=base_angle + math.pi/2, num=total_days / spawn_days)
+        outer_wall_angles = np.linspace(start=base_angle, stop=(base_angle +
+            math.pi/2), num=(total_days // spawn_days))
         self.midsorted_outer_wall_angles = midsort(outer_wall_angles)
 
-    def play(self, unit_id, unit_pos, map_states, current_scores, total_scores) -> [tuple[float, float]]:
+    def play(self, unit_id, unit_pos, map_states, current_scores, total_scores) -> List[Tuple[float, float]]:
         """Function which based on current game state returns the distance and angle of each unit active on the board
 
                 Args:
@@ -118,16 +121,16 @@ class Player:
                                                 move each unit of the player
                 """
 
-        while len(unit_id) > len(self.target_loc):
+        while len(unit_id[self.us]) > len(self.target_loc):
             # add new target_locations
-            self.target_loc.append(self.midsorted_outer_wall_angles[len(unit_id) - 1])
+            self.target_loc.append([self.initial_radius, self.midsorted_outer_wall_angles[len(unit_id) - 1]])
         
-        return self.get_moves(unit_pos, self.target_loc)
+        return get_moves(unit_pos[self.us], self.target_loc)
 
 def get_moves(unit_pos, target_loc) -> list[tuple[float, float]]:
-    assert(len(unit_pos) == len(target_loc), "get_moves: unit_pos and target_loc array length not the same")
-    np_unit_pos = np.array(unit_pos)
-    np_target_loc = np.array(target_loc)
+    assert len(unit_pos) == len(target_loc), "get_moves: unit_pos and target_loc array length not the same"
+    np_unit_pos = np.array(unit_pos, dtype=float)
+    np_target_loc = np.array(target_loc, dtype=float)
 
     cord_diff = np_target_loc - np_unit_pos
     cord_diff_x = cord_diff[:, 0]
