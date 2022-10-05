ebff53038080193ea322df6a5a13aaa0c98c6bc7
Mauricio Guerrero <65383117+Mg4145@users.noreply.github.com>
2022-10-04 21:35:59 -0400

    1st itr, taken from default -> Will change soon

--- players/g8_player.py
+++ players/g8_player.py
@@ -7,21 +7,29 @@ from typing import Tuple
 
 
 class Player:
-    def __init__(self, rng: np.random.Generator, logger: logging.Logger, total_days: int, spawn_days: int,
-                 player_idx: int, spawn_point: sympy.geometry.Point2D, min_dim: int, max_dim: int, precomp_dir: str) \
-            -> None:
+    def __init__(
+            self,
+            rng: np.random.Generator,
+            logger: logging.Logger,
+            total_days: int,
+            spawn_days: int,
+            player_idx: int,
+            spawn_point: sympy.geometry.Point2D,
+            min_dim: int,
+            max_dim: int,
+            precomp_dir: str) -> None:
         """Initialise the player with given skill.
 
-            Args:
-                rng (np.random.Generator): numpy random number generator, use this for same player behavior across run
-                logger (logging.Logger): logger use this like logger.info("message")
-                total_days (int): total number of days, the game is played
-                spawn_days (int): number of days after which new units spawn
-                player_idx (int): index used to identify the player among the four possible players
-                spawn_point (sympy.geometry.Point2D): Homebase of the player
-                min_dim (int): Minimum boundary of the square map
-                max_dim (int): Maximum boundary of the square map
-                precomp_dir (str): Directory path to store/load pre-computation
+        Args:
+            rng (np.random.Generator): numpy random number generator, use this for same player behavior across run
+            logger (logging.Logger): logger use this like logger.info("message")
+            total_days (int): total number of days, the game is played
+            spawn_days (int): number of days after which new units spawn
+            player_idx (int): index used to identify the player among the four possible players
+            spawn_point (sympy.geometry.Point2D): Homebase of the player
+            min_dim (int): Minimum boundary of the square map
+            max_dim (int): Maximum boundary of the square map
+            precomp_dir (str): Directory path to store/load pre-computation
         """
 
         # precomp_path = os.path.join(precomp_dir, "{}.pkl".format(map_path))
@@ -41,28 +49,64 @@ class Player:
 
         self.rng = rng
         self.logger = logger
+        self.total_days = total_days
+        self.spawn_days = spawn_days
+        self.player_idx = player_idx
+        self.spawn_point = spawn_point
+        self.min_dim = min_dim
+        self.max_dim = max_dim
+        self.precomp_dir = precomp_dir
 
-    def play(self, unit_id, unit_pos, map_states, current_scores, total_scores) -> [tuple[float, float]]:
+    def play(
+        self,
+        unit_id,
+        unit_pos,
+        map_states,
+        current_scores,
+        total_scores) -> [tuple[float, float]]:
         """Function which based on current game state returns the distance and angle of each unit active on the board
 
-                Args:
-                    unit_id (list(list(str))): contains the ids of each player's units (unit_id[player_idx][x])
-                    unit_pos (list(list(float))): contains the position of each unit currently present on the map
-                                                    (unit_pos[player_idx][x])
-                    map_states (list(list(int)): contains the state of each cell, using the x, y coordinate system
-                                                    (map_states[x][y])
-                    current_scores (list(int)): contains the number of cells currently occupied by each player
-                                                    (current_scores[player_idx])
-                    total_scores (list(int)): contains the cumulative scores up until the current day
-                                                    (total_scores[player_idx]
+        Args:
+            unit_id (list(list(str))): contains the ids of each player's units (unit_id[player_idx][x])
+            unit_pos (list(list(float))): contains the position of each unit currently present on the map
+                                            (unit_pos[player_idx][x])
+            map_states (list(list(int)): contains the state of each cell, using the x, y coordinate system
+                                            (map_states[x][y])
+            current_scores (list(int)): contains the number of cells currently occupied by each player
+                                            (current_scores[player_idx])
+            total_scores (list(int)): contains the cumulative scores up until the current day
+                                            (total_scores[player_idx]
 
-                Returns:
-                    List[Tuple[float, float]]: Return a list of tuples consisting of distance and angle in radians to
-                                                move each unit of the player
-                """
+        Returns:
+            List[Tuple[float, float]]: Return a list of tuples consisting of distance and angle in radians to
+                                        move each unit of the player
+        """
 
         moves = []
-        distance = sympy.Min(1, 100 - unit_pos.x) # move each troop outward in the form of a circle?
-        angle = sympy.atan2(100 - unit_pos.y, 100 - unit_pos.x) # this is right for us
-        moves.append((distance, angle))
+
+        # move each troop outward in the form of a circle?
+        # distance = sympy.Min(1, 100 - unit_pos.x)
+        # angle = sympy.atan2(100 - unit_pos.y, 100 - unit_pos.x)  # this is right for us
+        # moves.append((distance, angle))
+
+        for i in range(len(unit_id[self.player_idx])):
+            if self.player_idx == 0:
+                distance = sympy.Min(1, 100 - unit_pos[self.player_idx][i].x)
+                angle = sympy.atan2(100 - unit_pos[self.player_idx][i].y,
+                                    100 - unit_pos[self.player_idx][i].x)
+                moves.append((distance, angle))
+            elif self.player_idx == 1:
+                distance = sympy.Min(1, 100 - unit_pos[self.player_idx][i].x)
+                angle = sympy.atan2(0.5 - unit_pos[self.player_idx][i].y,
+                                    0.5 - unit_pos[self.player_idx][i].x)
+                moves.append((distance, angle))
+            elif self.player_idx == 2:
+                distance = sympy.Min(1, self.rng.random())
+                angle = sympy.atan2(-self.rng.random(), -self.rng.random())
+                moves.append((distance, angle))
+            else:
+                distance = sympy.Min(1, 0)
+                angle = sympy.atan2(0, 1)
+                moves.append((distance, angle))
+
         return moves
