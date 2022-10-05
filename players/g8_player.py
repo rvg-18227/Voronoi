df9d4a6be5c2fe70c9964380f114b015e3dae773
Xzh0u <xzh0u.sxz@gmail.com>
2022-10-05 12:32:42 -0400

    fix several problems for the algorithm of our player.

--- players/g8_player.py
+++ players/g8_player.py
@@ -2,14 +2,15 @@ import os
 import pickle
 import numpy as np
 import sympy
-from sympy import Point,Circle
+from sympy import Point, Circle
 import logging
-from typing import Tuple
+from typing import List, Tuple
 import math
 from shapely.geometry import Point
 from pyproj import Transformer
 from shapely.ops import transform
 import simplekml
+
 
 class Player:
     def __init__(
@@ -68,7 +69,7 @@ class Player:
             unit_pos,
             map_states,
             current_scores,
-            total_scores) -> [tuple[float, float]]:
+            total_scores) -> List[Tuple[float, float]]:
         """Function which based on current game state returns the distance and angle of each unit active on the board
 
         Args:
@@ -98,12 +99,12 @@ class Player:
         # if too big, shrink
         # if too small, expand
 
-        for i in range(len(unit_id[self.player_idx])):
-            if self.player_idx == 0:
-                distance = sympy.Min(1, 100 - unit_pos[self.player_idx][i].x)
-                angle = sympy.atan2(100 - unit_pos[self.player_idx][i].y,
-                                    100 - unit_pos[self.player_idx][i].x)
-                moves.append((distance, angle))
+        # for i in range(len(unit_id[self.player_idx])):
+        #     if self.player_idx == 0:
+        #         distance = sympy.Min(1, 100 - unit_pos[self.player_idx][i].x)
+        #         angle = sympy.atan2(100 - unit_pos[self.player_idx][i].y,
+        #                             100 - unit_pos[self.player_idx][i].x)
+        #         moves.append((distance, angle))
 #            elif self.player_idx == 1:
 #                distance = sympy.Min(1, 100 - unit_pos[self.player_idx][i].x)
 #                angle = sympy.atan2(0.5 - unit_pos[self.player_idx][i].y,
@@ -118,70 +119,83 @@ class Player:
 #                angle = sympy.atan2(0, 1)
 #                moves.append((distance, angle))
 
-        return moves
+        # return moves
 
 ###############################################################################
-#        points = unit_pos[self.player_idx]
-#        base_point = points[0]
-#        min_distance = 0.5
-#        f = 3
-#        t = self.total_days//self.spawn_days
-#        r = (f * self.max_dim ** 2 * 4 / math.pi)**(0.5)
-#        max_distance = math.pi * r / 2 * t
-#        if len(points) == 1:  # the day when we first spawn dont move
-#            distance = sympy.Min(1, 0)
-#            angle = sympy.atan2(0, 1)
-#            moves.append((distance, angle))
-#        elif len(points) == 2:
-#            # we have two units now!
-#
-#            t = self.total_days//self.spawn_days
-#            r = self.spawn_days * (self.max_dim ** 2) * f / (6 * t * math.pi ** 2)-0.5  # in this case 3 is just we are taking 1/3 of the area
-#            distance = sympy.Min(r)  # move each troop outward in the form of a circle?
-#            angle = sympy.atan2(100 - unit_pos.y, 100 - unit_pos.x)  # this is right for us
-#        else:
-#            # start spreading to other places
-#
-#            newest_point = points[-1]
-#            p_n, p_b = Point(newest_point), Point(base_point)
-#            point1 = points[1]
-#            p1 = Point(point1)
-#            current_radius = p_b.distance(p1)
-#            if (p_n.distance(p_b) == 0):
-#                # new point spanwed!!! time to spread :)
-#                current_radius += 1
-#                # some code to spread
-#                moves = self.spread_points(self, current_radius, points)
-#            step = 1
-#            point_dist_list = []
-#            for i in points(1, len(points)-1):
-#                p1, p2 = Point(points[i], points[i+1])
-#                point_dist_list.append(p1.distance(p2))
-#            if min(point_dist_list) <= min_distance or max(point_dist_list) >= max_distance:
-#                moves = self.spread_points(self, current_radius, points)
-#            else:
-#                # dont move
-#                distance = sympy.Min(1, 0)
-#                angle = sympy.atan2(0, 1)
-#                moves.append((distance, angle))
-#
-#        return moves
+        points = unit_pos[self.player_idx]
+        base_point = points[0]
+        min_distance = 0.5
+        f = 3
+        t = self.total_days//self.spawn_days
+        r = (f * self.max_dim ** 2 * 4 / math.pi)**(0.5)
+        max_distance = math.pi * r / 2 * t
+        if len(points) == 1:  # the day when we first spawn dont move
+            distance = sympy.Min(1, 0)
+            angle = sympy.atan2(0, 1)
+            moves.append((distance, angle))
+        elif len(points) == 2:
+            # we have two units now!
 
-#     def spread_points(self, radius: float, points: [tuple[float, float]]) -> [tuple[float, float]]:
-#         """Get the spread points.
+            t = self.total_days//self.spawn_days
+            # in this case 3 is just we are taking 1/3 of the area
+            r = self.spawn_days * (self.max_dim ** 2) * \
+                f / (6 * t * math.pi ** 2)-0.5
+            # move each troop outward in the form of a circle?
+            distance = sympy.Min(r)
+            angle1 = sympy.atan2(100 - points[0].y, 100 -
+                                 points[0].x)
+            angle2 = sympy.atan2(100 - points[1].y, 100 -
+                                 points[1].x)
+            moves.append((distance, angle1))
+            moves.append((distance, angle2))
+        else:
+            # start spreading to other places
+
+            newest_point = points[-1]
+            p_n, p_b = Point(newest_point), Point(base_point)
+            point1 = points[1]
+            p1 = Point(point1)
+            current_radius = p_b.distance(p1)
+            if (p_n.distance(p_b) == 0):
+                # new point spanwed!!! time to spread :)
+                current_radius += 1
+                # some code to spread
+                moves = self.spread_points(self, current_radius, points)
+            step = 1
+            point_dist_list = []
+            for i, item in enumerate(points):
+                if i == 0:
+                    continue
+                p1, p2 = points[i - 1], points[i]
+                point_dist_list.append(p1.distance(p2))
+            if min(point_dist_list) <= min_distance or max(point_dist_list) >= max_distance:
+                moves = self.spread_points(current_radius, points)
+            else:
+                # dont move
+                distance = sympy.Min(1, 0)
+                angle = sympy.atan2(0, 1)
+                moves.append((distance, angle))
+
+        return moves
+
+    def spread_points(self, radius: float, points: List[Tuple[float, float]]) -> List[Tuple[float, float]]:
+        """Get the spread points.
 #
-#         Args:
-#             radius (float): The radius to be used for circular spread
-#             points [tuple[float, float]]: List of points for the spread
-#
-#         Returns:
-#
-#         """
-#         # how each point shoudl move so that the points are evenly spread on the edge of the circle
-#         moves = []
-#         distance = sympy.Min(1, 100 - unit_pos.x) # move each troop outward in the form of a circle?
-#         angle = sympy.atan2(100 - unit_pos.y, 100 - unit_pos.x) # this is right for us
-#         moves.append((distance, angle))
-#
-#         return moves
+        Args:
+            radius (float): The radius to be used for circular spread
+            points [tuple[float, float]]: List of points for the spread
+
+        Returns:
+
+        """
+        # how each point shoudl move so that the points are evenly spread on the edge of the circle
+        moves = []
+        # move each troop outward in the form of a circle?
+        for item in points:
+            distance = sympy.Min(1, 100 - item.x)
+            angle = sympy.atan2(100 - item.y, 100 -
+                                item.x)  # this is right for us
+            moves.append((distance, angle))
+
+        return moves
 ###############################################################################
