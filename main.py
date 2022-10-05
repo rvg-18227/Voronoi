f828adad6328e18a7162c28e85e59913e5e76fe0
Jeff Tao <jatao88@gmail.com>
2022-10-05 00:17:49 -0400

    Fix args.disable_timeout error for --no_gui

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ if __name__ == '__main__':
     parser.add_argument("--log_path", default="log", help="Directory path to dump log files, filepath if "
                                                           "disable_logging is false")
     parser.add_argument("--disable_logging", action="store_true", help="Disable Logging, log_path becomes path to file")
+    parser.add_argument("--disable_timeout", action="store_true", help="Disable timeouts for player code")
     parser.add_argument("--player1", "-p1", default="d", help="Specifying player 1 out of 4")
     parser.add_argument("--player2", "-p2", default="d", help="Specifying player 2 out of 4")
     parser.add_argument("--player3", "-p3", default="d", help="Specifying player 3 out of 4")
