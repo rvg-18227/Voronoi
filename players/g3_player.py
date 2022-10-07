61d09b4dce75f754600235a901854c72ef4658ad
yunlanli <chnsidney@gmail.com>
2022-10-06 22:56:20 -0400

    move tests to separate file, add type annotations

--- players/g3_player.py
+++ players/g3_player.py
@@ -13,44 +13,6 @@ WALL_DENSITY = 0.1
 WALL_RATIO = 0
 
 
-def midsort(arr: list[float]) -> list[float]:
-    first_elem_added = False
-
-    n = len(arr)
-    if n <= 2:
-        return arr
-
-    prev_midpoints = [0, n - 1]
-    midsorted_arr = []
-
-    while len(prev_midpoints) < n:
-        curr_midpoints = []
-
-        for i, (left_pt, right_pt) in enumerate(zip(prev_midpoints, prev_midpoints[1:])):
-            mid_pt = (left_pt + right_pt) // 2
-            if mid_pt != left_pt or mid_pt == 0:
-                curr_midpoints.extend([left_pt, mid_pt])
-                midsorted_arr.append(arr[mid_pt])
-
-                if mid_pt == 0:
-                    first_elem_added = True
-
-            else:
-                curr_midpoints.append(left_pt)
-
-            if i == len(prev_midpoints) - 2:
-                curr_midpoints.append(right_pt)
-
-        prev_midpoints = curr_midpoints
-
-    # add the LAST element in the original @arr
-    if not first_elem_added:
-        midsorted_arr.append(arr[0])
-        
-    midsorted_arr.append(arr[-1])
-
-    return midsorted_arr
-
 class Player:
     def __init__(self, rng: np.random.Generator, logger: logging.Logger, total_days: int, spawn_days: int,
                  player_idx: int, spawn_point: sympy.geometry.Point2D, min_dim: int, max_dim: int, precomp_dir: str) \
@@ -141,7 +103,7 @@ class Player:
         return (x, y)
 
 
-def get_moves(unit_pos, target_loc) -> list[tuple[float, float]]:
+def get_moves(unit_pos: List[Tuple[float, float]], target_loc: List[Tuple[float, float]]) -> List[Tuple[float, float]]:
     assert len(unit_pos) == len(target_loc), "get_moves: unit_pos and target_loc array length not the same"
     np_unit_pos = np.array(unit_pos, dtype=float)
     np_target_loc = np.array(target_loc, dtype=float)
@@ -163,66 +125,39 @@ def shapely_pts_to_tuples(points: List[Point]) -> List[Tuple[float, float]]:
 def shapely_pt_to_tuple(point: Point) -> Tuple[float, float]:
     return ( float(point.x), float(point.y) )
 
+def midsort(arr: List[float]) -> List[float]:
+    n = len(arr)
+    if n <= 2:
+        return arr
 
-def test_midsort():
-    cases = [
-        {
-            'name': 'array_of_odd_number_of_elements',
-            'array': [1, 2, 3, 4, 5],
-            'expect': [3, 2, 4, 1, 5]
-        },
-        {
-            'name': 'corner_case_empty_array',
-            'array': [],
-            'expect': []
-        },
-        {
-            'name': 'array_of_single_element',
-            'array': [10],
-            'expect': [10]
-        },
-        {
-            'name': 'array_of_2_elements',
-            'array': [10, 30],
-            'expect': [10, 30]
-        },
-        {
-            'name': 'array_of_even_number_of_elements',
-            'array': [10, 78, 290, 208, 284, 285, 203, 173],
-            'expect': [208, 78, 285, 10, 290, 284, 203, 173]
-        }
-    ]
+    first_elem_added = False
+    prev_midpoints = [0, n - 1]
+    midsorted_arr = []
 
-    error_count = 0
+    while len(prev_midpoints) < n:
+        curr_midpoints = []
 
-    for tc in cases:
-        got = midsort(tc['array'])
+        for i, (left_pt, right_pt) in enumerate(zip(prev_midpoints, prev_midpoints[1:])):
+            mid_pt = (left_pt + right_pt) // 2
 
-        if got != tc['expect']:
-            print(f'case {tc["name"]} failed:')
-            print(f'expect: {tc["expect"]}')
-            print(f'got: {got}\n')
-            error_count += 1
+            if mid_pt != left_pt or mid_pt == 0:
+                curr_midpoints.extend([left_pt, mid_pt])
+                midsorted_arr.append(arr[mid_pt])
+
+                if mid_pt == 0:
+                    first_elem_added = True
+            else:
+                curr_midpoints.append(left_pt)
+
+            if i == len(prev_midpoints) - 2:
+                curr_midpoints.append(right_pt)
+
+        prev_midpoints = curr_midpoints
+
+    # add the LAST element in the original @arr
+    if not first_elem_added:
+        midsorted_arr.append(arr[0])
         
-    
-    if error_count == 0:
-       print("PASSED - test_midsort")
-    else:
-       print(f"FAILED with {error_count} errors - test_midsort")
-        
+    midsorted_arr.append(arr[-1])
 
-def test_get_moves():
-    cases = [
-        {
-            "unit_pos": [[0, 0], [0, 0], [0, 0]],
-            "target_loc": [[1, 1], [1, 4], [2, 5]]
-        }
-    ]
-    
-    result = get_moves(cases[0]['unit_pos'], cases[0]['target_loc'])
-    print("radians: " + str(result))
-
-    for i in range(len(result)):
-        result[i] = [result[i][0], result[i][1] * 180 / math.pi]
-    
-    print("degrees: " + str(result))
+    return midsorted_arr
