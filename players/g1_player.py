5a3775baf5fbb62ee74f761cf59b8a13b22f558a
Martin Ristovski <martin.ristovski@columbia.edu>
2022-10-07 20:42:29 -0400

    include code from simulator

--- players/g1_player.py
+++ players/g1_player.py
@@ -4,7 +4,55 @@ import numpy as np
 import sympy
 import logging
 from typing import Tuple
+from collections import defaultdict
 
+import matplotlib as mpl
+import shapely.errors
+import shapely.geometry
+import shapely.ops
+import scipy
+from matplotlib import pyplot as plt
+
+
+def delaunay2edges(tri_simplices):
+    """Convert the delaunay tris to unique edges
+    Args:
+        simplices: Triangles. The .simplices param of the triangulation object from scipy.
+    Ref:
+        https://stackoverflow.com/questions/69512972/how-to-generate-edge-index-after-delaunay-triangulation
+    """
+
+    def less_first(a, b):
+        return (a, b) if a < b else (b, a)
+
+    edges_dict = defaultdict(list)  # Gives all the edges and the associated triangles
+    for idx, triangle in enumerate(tri_simplices):
+        for e1, e2 in [[0, 1], [1, 2], [2, 0]]:  # for all edges of triangle
+            edge = less_first(triangle[e1], triangle[e2])  # always lesser index first
+            edges_dict[edge].append(idx)  # prevent duplicates. Track associated simplexes for each edge.
+
+    array_of_edges = np.array(list(edges_dict.keys()), dtype=int)
+    return array_of_edges
+
+def poly_are_neighbors(poly1: shapely.geometry.polygon.Polygon,
+                       poly2: shapely.geometry.polygon.Polygon) -> bool:
+    # Polygons are neighbors iff they share an edge. Only 1 vertex does not count.
+    # Also, both polygons might be the same
+    if isinstance(poly1.intersection(poly2), shapely.geometry.linestring.LineString):
+        return True
+    elif poly1 == poly2:
+        return True
+    else:
+        return False
+
+class Unit:
+    def __init__(self, player: int, pos: Tuple):
+        """The unit of each player. New unit spawns every N days. Can be moved in any direction by 1 km"""
+        assert 0 <= player < 4
+
+        self.player = int(player)
+        self.pos = pos
+        self.status = 1  # 1 = alive, 0 = dead
 
 class Player:
     def __init__(self, rng: np.random.Generator, logger: logging.Logger, total_days: int, spawn_days: int,
@@ -41,6 +89,16 @@ class Player:
 
         self.rng = rng
         self.logger = logger
+        self.precomp_dir = precomp_dir
+
+        self.player_idx = player_idx
+        self.spawn_point = spawn_point
+        self.min_dim = min_dim
+        self.max_dim = max_dim
+
+        self.total_days = total_days
+        self.spawn_days = spawn_days
+        self.current_day = 1
 
     def play(self, unit_id, unit_pos, map_states, current_scores, total_scores) -> [tuple[float, float]]:
         """Function which based on current game state returns the distance and angle of each unit active on the board
@@ -63,4 +121,144 @@ class Player:
 
         moves = []
 
+        map_size = 100
+        home_offset = 0.5
+        _MAP_W = map_size
+        spawn_loc = {0: (home_offset, home_offset),
+                    1: (_MAP_W - home_offset, home_offset),
+                    2: (_MAP_W - home_offset, _MAP_W - home_offset),
+                    3: (home_offset, _MAP_W - home_offset)}
+
+        # Construct 2 lists for passing to shapely/scipy for triangulation
+        # Handle edge case where cell is occupied by 2 players: Quantize pts to grid cells.
+        #   When a cell is disputed, it no longer contributes to the voronoi diagram. All the units within that cell must
+        #   be removed.
+        units = []
+        for id in unit_id[self.player_idx]:
+            units.append((self.player_idx, unit_pos[self.player_idx][id]))
+
+        pts_hash = {}
+        for u in units:
+            # Quantize unit pos to cell. We assume cell origin at center.
+            pos_int = (int(u.pos[0]) + home_offset, int(u.pos[1]) + home_offset)
+            if pos_int in pts_hash:
+                player_existing = pts_hash[pos_int]
+                if player_existing == u.player:
+                    pts_hash[pos_int] = u.player
+                else:
+                    pass  # Disputed cell
+            else:
+                pts_hash[pos_int] = u.player
+        pts = list(pts_hash.keys())
+        player_ids = list(pts_hash.values())
+
+        # Get polygon of Voronoi regions around each pt
+        _points = shapely.geometry.MultiPoint(pts)
+        envelope = shapely.geometry.box(0, 0, map_size, map_size)
+        vor_regions_ = shapely.ops.voronoi_diagram(_points, envelope=envelope)
+        vor_regions_ = list(vor_regions_)  # Convert to a list of Polygon
+
+        # The polys aren't being bounded correctly. Fix manually.
+        vor_regions = []
+        for region in vor_regions_:
+            if not isinstance(region, shapely.geometry.Polygon):
+                print(f"WARNING: Region returned from voronoi not a polygon: {type(region)}")
+
+            region_bounded = region.intersection(envelope)
+            if region_bounded.area > 0:
+                vor_regions.append(region_bounded)
+
+        # Add the home base to list of points
+        pts_with_home = pts.copy()
+        player_ids_with_home = player_ids.copy()
+        for player in range(4):
+            # Add home bases as pts
+            pts_with_home.append(spawn_loc[player])
+            player_ids_with_home.append(player)
+
+        # Find mapping from pts idx to polys (via nearest unit) and poly to player
+        pt_to_poly = {}  # includes home base
+        # Polygon isn't hashable, so we use polygon idx.
+        poly_idx_to_player = {}
+        kdtree = scipy.spatial.KDTree(pts)
+        for region_idx, region in enumerate(vor_regions):
+            # Voronoi regions are all convex. Nearest pt to centroid must be point belonging to region
+            # centroid = region.centroid.coords[:][0]
+            # _, ii = kdtree.query(centroid, k=1)  # index of nearest pt
+            repr_pt = region.representative_point()
+            _, ii = kdtree.query(repr_pt, k=1)  # index of nearest pt
+
+            pt_to_poly[pts[ii]] = region_idx
+            poly_idx_to_player[region_idx] = player_ids[ii]
+
+        # Find mapping of each home base to poly
+        for idx in range(4):
+            home_coord = spawn_loc[idx]
+            _, ii = kdtree.query(home_coord, k=1)  # index of nearest pt
+            pt_to_poly[home_coord] = pt_to_poly[pts[ii]]  # home base same as nearest unit
+
+        # Get the graph of connected pts via triangulation (include home base when triangulating)
+        pts_with_home = pts.copy()
+        player_ids_with_home = player_ids.copy()
+        for key, val in spawn_loc.items():
+            # Add home bases as pts
+            pts_with_home.append(val)
+            player_ids_with_home.append(key)
+
+        tri = scipy.spatial.Delaunay(np.array(pts_with_home))
+        edges = delaunay2edges(tri.simplices)  # Shape: [N, 2]
+
+        # Clean edges
+        # TODO: Handle case when enemy unit within home cell. It will cut off all player's units.
+        #  Soln: When making graph, remove edge.
+        #  Problem: How will we do a path search to home base?
+        edge_player_id = []  # Player each edge belongs to
+        for p1, p2 in edges:
+            player1 = player_ids_with_home[p1]
+            player2 = player_ids_with_home[p2]
+
+            valid_ = False
+            if player1 == player2:
+                poly1_idx = pt_to_poly[tuple(pts_with_home[p1])]
+                poly2_idx = pt_to_poly[tuple(pts_with_home[p2])]
+                poly1 = vor_regions[poly1_idx]
+                poly2 = vor_regions[poly2_idx]
+
+                # The polygons must both belong to the same player
+                # This handles edge cases where home base is conquered by another player
+                play1_ = poly_idx_to_player[poly1_idx]
+                play2_ = poly_idx_to_player[poly2_idx]
+
+                are_neighbors = poly_are_neighbors(poly1, poly2)
+                if are_neighbors and play1_ == player1 and play2_ == player1:
+                    # Can traverse edge only if voronoi polys are neighbors
+                    valid_ = True
+
+            if valid_:
+                edge_player_id.append(player1)
+            else:
+                edge_player_id.append(-1)
+
+        edge_player_id = np.array(edge_player_id)
+        mask_edges = (edge_player_id > -1)
+
+        # Remove invalid edges
+        edges = edges[mask_edges]
+        edge_player_id = edge_player_id[mask_edges]
+
+        # TODO: BUILD A LIST OF NEIGHBORING POLYGONS FOR EVERY POLYGON
+
+        # Build a graph data structure for each player
+        graphs = {0: defaultdict(list), 1: defaultdict(list), 2: defaultdict(list), 3: defaultdict(list)}
+        for player, (p1, p2) in zip(edge_player_id, edges):
+            if player > -1:
+                graph_p = graphs[player]
+                graph_p[p1].append(p2)
+                graph_p[p2].append(p1)
+
+        # TODO: From each home base, traverse the full graph
+
+
+
+        self.current_day += 1
         return moves
