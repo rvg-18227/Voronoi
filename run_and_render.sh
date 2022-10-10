a51f1a8d0eb0c4bfeaf8e9a77417f493cdfb849a
rvg-18227 <63065210+rvg-18227@users.noreply.github.com>
2022-10-10 03:32:36 -0400

    Delete run_and_render.sh

--- run_and_render.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-python3 main.py -p1 2 -p2 2 -p3 2 -p4 2 --no_gui --dump_state --last 50
-echo "Rendering frames..."
-python render_game.py
-echo "Creating video..."
-convert -delay 5 -loop 0 $(ls -1 render/*.png | sort -V) -quality 95 game.mp4
