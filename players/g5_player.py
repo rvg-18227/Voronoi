32c43c9182ad9d7ad208829b419b7008120a0e82
yiwei-w <redroomsd@gmail.com>
2022-10-09 22:56:58 -0400

    Cluster strategy

--- players/g5_player.py
+++ players/g5_player.py
@@ -76,6 +76,145 @@ class Player:
     def attractive_force(self, p1, p2):
         return -self.repelling_force(p1, p2)
 
+
+    def initial_strategy(self, unit_id, unit_pos, map_states, current_scores, total_scores, own_units, enemy_units_locations):
+        ENEMY_INFLUENCE = 1
+        HOME_INFLUENCE = 20
+        ALLY_INFLUENCE = 0.6
+        BOUNDARY_INFLUENCE = 1
+        BOUNDARY_THRESHOLD = 1
+        BOUNDARY_FACTOR = 10
+
+        
+        enemy_unit_forces = [
+            self.attractive_force(unit_pos, enemy_pos)
+            for _, enemy_pos in enemy_units_locations
+        ]
+        enemy_force = np.add.reduce(enemy_unit_forces)
+
+        ally_force = [
+            self.repelling_force(unit_pos, ally_pos)
+            for ally_id, ally_pos in own_units
+            if ally_id != unit_id
+        ]
+        ally_force = np.add.reduce(ally_force)
+
+        boundary_force = np.array([0.0, 0.0])
+        top_repelling_force = np.array([0.0, 1.0])
+        bottom_repelling_force = np.array([0.0, -1.0])
+        left_repelling_force = np.array([1.0, 0.0])
+        right_repelling_force = np.array([-1.0, 0.0])
+        if unit_pos[0] < BOUNDARY_THRESHOLD:
+            if unit_pos[0] > 0:
+                boundary_force += left_repelling_force * (1.0 / unit_pos[0])
+            else:
+                boundary_force += left_repelling_force * BOUNDARY_FACTOR
+        if unit_pos[0] > 100 - BOUNDARY_THRESHOLD:
+            if unit_pos[0] < 100:
+                boundary_force += right_repelling_force * (1.0 / (100.0 - unit_pos[0]))
+            else:
+                boundary_force += right_repelling_force * BOUNDARY_FACTOR
+        if unit_pos[1] < BOUNDARY_THRESHOLD:
+            if unit_pos[1] > 0:
+                boundary_force += top_repelling_force * (1.0 / unit_pos[1])
+            else:
+                boundary_force += top_repelling_force * BOUNDARY_FACTOR
+        if unit_pos[1] > 100 - BOUNDARY_THRESHOLD:
+            if unit_pos[1] < 100:
+                boundary_force += bottom_repelling_force * (1.0 / (100.0 - unit_pos[1]))
+            else:
+                boundary_force += bottom_repelling_force * BOUNDARY_FACTOR
+        
+        if unit_pos[0] == self.homebase[0] and unit_pos[1] == self.homebase[1]:
+            home_force = np.array([0.0, 0.0])
+        else:
+            home_force = self.repelling_force(unit_pos, self.homebase)
+        
+
+        total_force = self.normalize(
+            (enemy_force * ENEMY_INFLUENCE)
+            + (home_force * HOME_INFLUENCE)
+            + (ally_force * ALLY_INFLUENCE)
+            + (boundary_force * BOUNDARY_INFLUENCE)
+        )
+
+        return self.to_polar(total_force)
+
+    def cluster_strategy(self, unit_id, unit_pos, map_states, current_scores, total_scores, own_units, enemy_units_locations, closest_cluster, closest_cluster_distance):
+        row, col, ids = closest_cluster
+        ENEMY_INFLUENCE = 1
+        HOME_INFLUENCE = 20
+        ALLY_INFLUENCE = 0.2
+        CLUSTER_ENEMY_INFLUENCE = 5 * (1 / closest_cluster_distance)
+        BOUNDARY_INFLUENCE = 1
+        BOUNDARY_THRESHOLD = 1
+        BOUNDARY_FACTOR = 10
+        
+        cluster_enemy_unit_forces = [
+            self.repelling_force(unit_pos, enemy_pos)
+            for enemy_id, enemy_pos in enemy_units_locations
+            if enemy_id in ids
+        ]
+        cluster_enemy_force = np.add.reduce(cluster_enemy_unit_forces)
+
+        other_enemy_unit_forces = [
+            self.attractive_force(unit_pos, enemy_pos)
+            for enemy_id, enemy_pos in enemy_units_locations
+            if enemy_id not in ids
+        ]
+        other_enemy_force = np.add.reduce(other_enemy_unit_forces)
+
+        ally_force = [
+            self.repelling_force(unit_pos, ally_pos)
+            for ally_id, ally_pos in own_units
+            if ally_id != unit_id
+        ]
+        ally_force = np.add.reduce(ally_force)
+
+        boundary_force = np.array([0.0, 0.0])
+        top_repelling_force = np.array([0.0, 1.0])
+        bottom_repelling_force = np.array([0.0, -1.0])
+        left_repelling_force = np.array([1.0, 0.0])
+        right_repelling_force = np.array([-1.0, 0.0])
+        if unit_pos[0] < BOUNDARY_THRESHOLD:
+            if unit_pos[0] > 0:
+                boundary_force += left_repelling_force * (1.0 / unit_pos[0])
+            else:
+                boundary_force += left_repelling_force * BOUNDARY_FACTOR
+        if unit_pos[0] > 100 - BOUNDARY_THRESHOLD:
+            if unit_pos[0] < 100:
+                boundary_force += right_repelling_force * (1.0 / (100.0 - unit_pos[0]))
+            else:
+                boundary_force += right_repelling_force * BOUNDARY_FACTOR
+        if unit_pos[1] < BOUNDARY_THRESHOLD:
+            if unit_pos[1] > 0:
+                boundary_force += top_repelling_force * (1.0 / unit_pos[1])
+            else:
+                boundary_force += top_repelling_force * BOUNDARY_FACTOR
+        if unit_pos[1] > 100 - BOUNDARY_THRESHOLD:
+            if unit_pos[1] < 100:
+                boundary_force += bottom_repelling_force * (1.0 / (100.0 - unit_pos[1]))
+            else:
+                boundary_force += bottom_repelling_force * BOUNDARY_FACTOR
+        
+        if unit_pos[0] == self.homebase[0] and unit_pos[1] == self.homebase[1]:
+            home_force = np.array([0.0, 0.0])
+        else:
+            home_force = self.repelling_force(unit_pos, self.homebase)
+        
+
+        total_force = self.normalize(
+            + (cluster_enemy_force * CLUSTER_ENEMY_INFLUENCE)
+            + (other_enemy_force * ENEMY_INFLUENCE)
+            + (home_force * HOME_INFLUENCE)
+            + (ally_force * ALLY_INFLUENCE)
+            + (boundary_force * BOUNDARY_INFLUENCE)
+        )
+
+        return self.to_polar(total_force)
+
+
+
     def play(self, unit_id, unit_pos, map_states, current_scores, total_scores) -> [tuple[float, float]]:
         """Function which based on current game state returns the distance and angle of each unit active on the board
 
@@ -101,6 +240,7 @@ class Player:
                 [sympy_p_float(pos) for pos in unit_pos[self.player_idx]],
             )
         )
+
         enemy_units_locations = [
             (i, sympy_p_float(unit_pos[player][i]))
             for player in range(len(unit_pos))
@@ -108,57 +248,38 @@ class Player:
             if player != self.player_idx
         ]
 
-        ENEMY_INFLUENCE = 1
-        HOME_INFLUENCE = 20
-        ALLY_INFLUENCE = 0.5
-        BOUNDARY_INFLUENCE = 1
-        BORDER_THRESHOLD = 2
-
+        BLOCK_SIZE = 4
+        CLUSTER_THRESHOLD = 10
+        block_count =  [[set()] * (100 // BLOCK_SIZE) for i in range(100 // BLOCK_SIZE)]
+        for enemy_id, enemy_pos in enemy_units_locations:
+            col, row = int(enemy_pos[0]), int(enemy_pos[1])
+            block_count[row // BLOCK_SIZE][col // BLOCK_SIZE].add(enemy_id)
+        
+        clusters = []
+        for row in range(100 // BLOCK_SIZE):
+            for col in range(100 // BLOCK_SIZE):
+                if len(block_count[row][col]) >= CLUSTER_THRESHOLD:
+                    clusters.append((row, col, block_count[row][col]))
+                
         moves = []
-
-        for unit_id, unit_pos in own_units:
-            enemy_unit_forces = [
-                self.attractive_force(unit_pos, enemy_pos)
-                for _, enemy_pos in enemy_units_locations
-            ]
-            enemy_force = np.add.reduce(enemy_unit_forces)
-
-            ally_force = [
-                self.repelling_force(unit_pos, ally_pos)
-                for ally_id, ally_pos in own_units
-                if ally_id != unit_id
-            ]
-            ally_force = np.add.reduce(ally_force)
-
-            boundary_force = np.array([0.0, 0.0])
-            top_repelling_force = np.array([0.0, 1.0])
-            bottom_repelling_force = np.array([0.0, -1.0])
-            left_repelling_force = np.array([1.0, 0.0])
-            right_repelling_force = np.array([-1.0, 0.0])
-            if unit_pos[0] < BORDER_THRESHOLD:
-                # print(left_repelling_force)
-                # print(unit_pos)
-                boundary_force += left_repelling_force * (1.0 / unit_pos[0])
-            if unit_pos[0] > 100 - BORDER_THRESHOLD:
-                boundary_force += right_repelling_force * (1.0 / (100.0 - unit_pos[0]))
-            if unit_pos[1] < BORDER_THRESHOLD:
-                boundary_force += top_repelling_force * (1.0 / unit_pos[1])
-            if unit_pos[1] > 100 - BORDER_THRESHOLD:
-                boundary_force += bottom_repelling_force * (1.0 / (100.0 - unit_pos[1]))
-            
-            if unit_pos[0] == self.homebase[0] and unit_pos[1] == self.homebase[1]:
-                home_force = np.array([0.0, 0.0])
-            else:
-                home_force = self.repelling_force(unit_pos, self.homebase)
-            
-
-            total_force = self.normalize(
-                (enemy_force * ENEMY_INFLUENCE)
-                + (home_force * HOME_INFLUENCE)
-                + (ally_force * ALLY_INFLUENCE)
-                + (boundary_force * BOUNDARY_INFLUENCE)
-            )
-
-            moves.append(self.to_polar(total_force))
-
+        if len(clusters) <= 3:
+            for unit_id, unit_pos in own_units:
+                moves.append(self.initial_strategy(unit_id, unit_pos, map_states, current_scores, total_scores, own_units, enemy_units_locations))
+        else:
+            for unit_id, unit_pos in own_units:
+                closest_cluster_distance = float("inf")
+                closest_cluster = None
+                for (row, col, ids) in clusters:
+                    cluster_center = np.array([col * BLOCK_SIZE + BLOCK_SIZE / 2, row * BLOCK_SIZE + BLOCK_SIZE / 2])
+                    if np.linalg.norm(cluster_center - unit_pos) < closest_cluster_distance:
+                        closest_cluster_distance = np.linalg.norm(cluster_center - unit_pos)
+                        closest_cluster = (row, col, ids)
+                
+                if closest_cluster_distance > 20:
+                    moves.append(self.initial_strategy(unit_id, unit_pos, map_states, current_scores, total_scores, 
+                                                       own_units, enemy_units_locations))
+                else:
+                    moves.append(self.cluster_strategy(unit_id, unit_pos, map_states, current_scores, total_scores,
+                                                       own_units, enemy_units_locations, closest_cluster, closest_cluster_distance))
         return moves
+
