eb3f6192a744b4727c90e437a398bbba1444f8b2
Maximo Oen <mmo2134@columbia.edu>
2022-10-10 06:56:42 -0400

    Created secondary player that uses frontiers and interest regions -Fast, but probably should create regions dynamically (?)

---
+++ run_and_render.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+python3 main.py -p1 2 -p2 9 --no_gui --dump_state --spawn 10 --last 150
+echo "Rendering frames..."
+python render_game.py
+echo "Creating video..."
+convert -delay 5 -loop 0 $(ls -1 render/*.png | sort -V) -quality 95 game.mp4
