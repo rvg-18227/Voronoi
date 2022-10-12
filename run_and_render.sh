130abcd45959cb01819c5037fb809e540e111bab
rvg-18227 <63065210+rvg-18227@users.noreply.github.com>
2022-10-12 11:15:48 -0400

    Clearing conflicts

--- run_and_render.sh
+++ run_and_render.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-python3 main.py -p1 2 -p2 8 -p3 3 -p4 4 --no_gui --dump_state --last 250
+python main.py -p1 2 -p2 8 -p3 3 -p4 4 --no_gui --dump_state --last 250
 echo "Rendering frames..."
-python3 render_game.py
+python render_game.py
 echo "Creating video..."
 convert -delay 5 -loop 0 $(ls -1 render/*.png | sort -V) -quality 95 game.mp4
