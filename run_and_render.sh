0fdebe7b94d5bdca81322c28de5863043db1a80f
yiwei-w <redroomsd@gmail.com>
2022-10-17 16:14:35 -0400

    Add render script

--- run_and_render.sh
+++ run_and_render.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-python main.py --dump_state --no_gui
+python main.py --dump_state --no_gui --last 1000 --spawn 20 --player1 8 --player2 7 --player3 6 --player4 5
 echo "Rendering frames..."
 python render_game.py
 echo "Creating video..."
