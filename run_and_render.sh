c4b0504be6cfe0e1e1abc63adf8c9c910b24d539
rvg-18227 <63065210+rvg-18227@users.noreply.github.com>
2022-10-19 16:57:18 -0400

    Update run_and_render.sh

--- run_and_render.sh
+++ run_and_render.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-python3 main.py --dump_state --no_gui --player1 8 --player2 d --player3 9  --last 50
+python main.py --dump_state --no_gui --player1 8 --player2 d --player3 9  --last 50
 echo "Rendering frames..."
-python3 render_game.py
+python render_game.py
 echo "Creating video..."
 convert -delay 5 -loop 0 $(ls -1 render/*.png | sort -V) -quality 95 game.mp4
